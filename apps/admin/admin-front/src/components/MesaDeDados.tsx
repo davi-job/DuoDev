7afd96425408466dc4160804b79467c7ce5108aa
davijob <davi.hjobr@gmail.com>
2026-03-26 15:34:16 -0300

    REFACT: atualizar a mesa de dados para ela ser modular

--- apps/admin/admin-front/src/components/MesaDeDados.tsx
+++ apps/admin/admin-front/src/components/MesaDeDados.tsx
@@ -1,24 +1,17 @@
 import BotaoAcoes from './BotaoAcoes';
 import './MesaDeDados.css';
-import StatusPill from './StatusPill';
 
-const dadosTeste = {
-    headers: ['CATEGORIA', 'STATUS', 'TRILHAS', 'CLASSES', 'QUESTÕES', 'AÇÕES'],
-    data: [
-        ['FrontEnd', <StatusPill cor="verde" texto="Publicado" />, 3, 34, 59],
-        ['BackEnd', <StatusPill cor="amarelo" texto="Rascunho" />, 1, 12, 23],
-        ['Segurança', <StatusPill cor="vermelho" texto="Revisão" />, 5, 82, 123],
-        ['Segurança', <StatusPill cor="verde" texto="Publicado" />, 2, 90, 46],
-        ['Segurança', <StatusPill cor="azul" texto="Arquivado" />, 4, 67, 82],
-    ],
-};
+interface mesaProps {
+    headers: Array<string>;
+    data: Array<Array<any>>;
+}
 
-function MesaDeDados() {
+function MesaDeDados({ headers, data }: mesaProps) {
     return (
         <table>
             <thead>
                 <tr>
-                    {dadosTeste.headers.map((header, i) => (
+                    {headers.map((header, i) => (
                         <th key={i}>
                             <h4>{header}</h4>
                         </th>
@@ -27,7 +20,7 @@ function MesaDeDados() {
             </thead>
 
             <tbody>
-                {dadosTeste.data.map((row, i) => (
+                {data.map((row, i) => (
                     <tr key={i}>
                         {row.map((cell, j) => (
                             <td key={j}>{cell}</td>
--- apps/admin/admin-front/src/routes/(modules)/Categorias.tsx
+++ apps/admin/admin-front/src/routes/(modules)/Categorias.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from '@tanstack/react-router';
 
 import MesaDeDados from '../../components/MesaDeDados';
 import ModuleHeader from '../../components/ModuleHeader';
+import StatusPill from '../../components/StatusPill';
 
 import './Categorias.css';
 
@@ -16,11 +17,20 @@ function Categorias() {
                 path={['Admin', 'Categorias']}
                 title={'Categorias'}
                 btnLabel="Nova Categoria"
-                btnOnClick={() => console.log('Clicked')}
+                btnOnClick={() => console.log('Nova Categoria Criada')}
             />
 
             <section className="data">
-                <MesaDeDados />
+                <MesaDeDados
+                    headers={['CATEGORIA', 'STATUS', 'TRILHAS', 'CLASSES', 'QUESTÕES', 'AÇÕES']}
+                    data={[
+                        ['FrontEnd', <StatusPill cor="verde" texto="Publicado" />, 3, 34, 59],
+                        ['BackEnd', <StatusPill cor="amarelo" texto="Rascunho" />, 1, 12, 23],
+                        ['Segurança', <StatusPill cor="vermelho" texto="Revisão" />, 5, 82, 123],
+                        ['Mobile', <StatusPill cor="verde" texto="Publicado" />, 2, 90, 46],
+                        ['AI', <StatusPill cor="azul" texto="Arquivado" />, 4, 67, 82],
+                    ]}
+                />
             </section>
         </>
     );
