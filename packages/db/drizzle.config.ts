103b5f64c5610f63da9141b20d0674c131e5fb3e
davi-job <davi.hjobr@gmail.com>
2026-05-09 18:08:17 -0300

    FIX: remover arquivos e configurações do banco de dados temporário

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,11 +1,14 @@
-import 'dotenv/config';
+import { config } from 'dotenv';
+import path from 'path';
 import { defineConfig } from 'drizzle-kit';
+
+config({ path: path.resolve(__dirname, '../../.env') });
 
 export default defineConfig({
     schema: './src/schema/index.ts',
     out: './migrations',
-    driver: 'pg',
+    dialect: 'postgresql',
     dbCredentials: {
-        connectionString: process.env.DATABASE_URL!,
+        url: process.env.DATABASE_URL!,
     },
 });
