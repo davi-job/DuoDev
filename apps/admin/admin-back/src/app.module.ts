0fbca7949d45650a0e493746ec67cec2a5ae058e
davi-job <davi.hjobr@gmail.com>
2026-05-09 18:47:52 -0300

    FEAT: adicionar módulos da database e do app

--- apps/admin/admin-back/src/app.module.ts
+++ apps/admin/admin-back/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { CategoriesModule } from './categories/categories.module';
+import { TrailsModule } from './trails/trails.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [DatabaseModule, CategoriesModule, TrailsModule],
 })
 export class AppModule {}
